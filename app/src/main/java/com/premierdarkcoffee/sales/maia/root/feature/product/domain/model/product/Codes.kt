5d59918796a5f7d90fb362c307d8c321c60d82c7
Jose Ruiz <ruiz211@protonmail.com>
2025-02-05 22:36:58 -0500

    Maia update

--- app/src/main/java/com/premierdarkcoffee/sales/maia/root/feature/product/domain/model/product/Codes.kt
+++ app/src/main/java/com/premierdarkcoffee/sales/maia/root/feature/product/domain/model/product/Codes.kt
@@ -1,5 +1,7 @@
 package com.premierdarkcoffee.sales.maia.root.feature.product.domain.model.product
 
+import com.premierdarkcoffee.sales.maia.root.feature.product.data.remote.dto.CodesDto
+
 data class Codes(val EAN: String) {
 
     fun toCodesDto(): CodesDto {
