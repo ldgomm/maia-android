84d5d2ae3d3c2b3ab9c2c28bdef2e12604fd5ec1
Jose Ruiz <ruiz211@protonmail.com>
2025-02-05 22:37:00 -0500

    Maia update

--- app/src/main/java/com/premierdarkcoffee/sales/maia/root/feature/chat/data/local/entity/product/CodesEntity.kt
+++ app/src/main/java/com/premierdarkcoffee/sales/maia/root/feature/chat/data/local/entity/product/CodesEntity.kt
@@ -1,3 +1,10 @@
 package com.premierdarkcoffee.sales.maia.root.feature.chat.data.local.entity.product
 
-data class CodesEntity(val EAN: String)
+import com.premierdarkcoffee.sales.maia.root.feature.product.domain.model.product.Codes
+
+data class CodesEntity(val EAN: String) {
+
+    fun toCodes(): Codes {
+        return  Codes(EAN)
+    }
+}
