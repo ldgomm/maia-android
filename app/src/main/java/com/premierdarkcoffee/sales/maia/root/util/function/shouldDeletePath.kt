fb565c7f7c6208afdb1dc81889762512c0466875
Jose Ruiz <ruiz211@protonmail.com>
2025-02-26 20:39:01 -0500

    Maia update

--- app/src/main/java/com/premierdarkcoffee/sales/maia/root/util/function/shouldDeletePath.kt
+++ app/src/main/java/com/premierdarkcoffee/sales/maia/root/util/function/shouldDeletePath.kt
@@ -1,6 +1,6 @@
 package com.premierdarkcoffee.sales.maia.root.util.function
 
 fun shouldDeletePath(path: String): Boolean {
-    val basePath = "sales/stores"
+    val basePath = "fake/stores"
     return path.startsWith("$basePath/") && path.length > basePath.length
 }
