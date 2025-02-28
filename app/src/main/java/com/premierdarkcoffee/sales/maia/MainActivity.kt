b6ff8d5f0b29d8c39a1e99407a8e32ef423a3514
Jose Ruiz <ruiz211@protonmail.com>
2025-02-28 18:30:54 -0500

    Maia update

--- app/src/main/java/com/premierdarkcoffee/sales/maia/MainActivity.kt
+++ app/src/main/java/com/premierdarkcoffee/sales/maia/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.auth.FirebaseAuth
@@ -16,32 +18,31 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-//    private lateinit var networkMonitor: NetworkMonitor
+    private lateinit var networkMonitor: NetworkMonitor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val apiKey = BuildConfig.API_KEY
         ApiKeySecurePreferencesHelper.storeApiKey(this, apiKey)
 
-//        networkMonitor = NetworkMonitor(this)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-
         enableEdgeToEdge()
+        networkMonitor = NetworkMonitor(this)
+
         setContent {
             val navController = rememberNavController()
             val user = FirebaseAuth.getInstance().currentUser
             val startDestination = if (user != null) ProductsRoute else AuthenticationRoute
-//            val isConnected by networkMonitor.observeAsState(true)
+            val isConnected by networkMonitor.observeAsState(true)
 
             MaiaTheme {
-//                when {
-//                    !isConnected -> NoInternetView()
-                // Add logic to handle unstable connection if needed
-//                    user != null ->
-                MainView(navController = navController, startDestination = startDestination)
-//                    else -> {
-//                    }
-//                }
+                when {
+                    !isConnected -> NoInternetView()
+                    user != null -> MainView(navController = navController, startDestination = startDestination)
+                    else -> {
+                        UnstableConnectionView()
+                    }
+                }
             }
         }
     }
