95a34f9177af145bedb885b147e78e0fc623261f
Jose Ruiz <ruiz211@protonmail.com>
2025-03-08 23:55:39 -0500

    Maia update

--- app/src/main/java/com/premierdarkcoffee/sales/maia/root/feature/chat/data/local/entity/product/PriceEntity.kt
+++ app/src/main/java/com/premierdarkcoffee/sales/maia/root/feature/chat/data/local/entity/product/PriceEntity.kt
@@ -1,6 +1,5 @@
 package com.premierdarkcoffee.sales.maia.root.feature.chat.data.local.entity.product
 
-import com.premierdarkcoffee.sales.hermes.root.feature.chat.data.local.entity.product.OfferEntity
 import com.premierdarkcoffee.sales.maia.root.feature.product.domain.model.product.Price
 
 data class PriceEntity(val amount: Double,
