d9f13bc1c4e10b0932245b1b0f76c418a8b96820
Jose Ruiz <ruiz211@protonmail.com>
2025-03-21 13:22:29 -0500

    Maia update

--- app/src/main/java/com/premierdarkcoffee/sales/maia/root/feature/product/domain/usecase/GetGroupsUseCase.kt
+++ app/src/main/java/com/premierdarkcoffee/sales/maia/root/feature/product/domain/usecase/GetGroupsUseCase.kt
@@ -1,13 +1,13 @@
 package com.premierdarkcoffee.sales.maia.root.feature.product.domain.usecase
 
-import com.premierdarkcoffee.sales.maia.root.feature.product.domain.serviceable.DataServiceable
+import com.premierdarkcoffee.sales.maia.root.feature.product.domain.serviceable.GroupServiceable
 import com.premierdarkcoffee.sales.maia.root.feature.product.domain.serviceable.Group
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetGroupsUseCase @Inject constructor(private val dataServiceable: DataServiceable) {
+class GetGroupsUseCase @Inject constructor(private val groupServiceable: GroupServiceable) {
 
     operator fun invoke(url: String): Flow<Result<List<Group>>> {
-        return dataServiceable.getGroups(url)
+        return groupServiceable.getGroups(url)
     }
 }
