e8ab172900b7fa84833a70023db085a0e82e6dfb
Jose Ruiz <ruiz211@protonmail.com>
2025-03-22 13:22:36 -0500

    Maia update

--- app/src/main/java/com/premierdarkcoffee/sales/maia/root/feature/product/data/remote/service/GroupService.kt
+++ app/src/main/java/com/premierdarkcoffee/sales/maia/root/feature/product/data/remote/service/GroupService.kt
@@ -1,14 +1,9 @@
-package com.premierdarkcoffee.sales.maia.root.feature.product.domain.serviceable
+package com.premierdarkcoffee.sales.maia.root.feature.product.data.remote.service
 
-//
-//  DataService.kt
-//  Maia
-//
-//  Created by José Ruiz on 25/9/24.
-//
-
-import android.content.ContentValues.TAG
+import android.content.ContentValues
 import android.util.Log
+import com.premierdarkcoffee.sales.maia.root.feature.product.domain.serviceable.Group
+import com.premierdarkcoffee.sales.maia.root.feature.product.domain.serviceable.GroupServiceable
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
@@ -16,16 +11,13 @@ import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.ContentType
-import io.ktor.http.HttpStatusCode.Companion.Created
-import io.ktor.http.HttpStatusCode.Companion.Found
-import io.ktor.http.HttpStatusCode.Companion.NotFound
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-
-class DataService @Inject constructor(private val httpClient: HttpClient) : DataServiceable {
+class GroupService @Inject constructor(private val httpClient: HttpClient) : GroupServiceable {
 
     override fun addGroup(url: String, group: Group): Flow<Result<Group>> {
         return flow {
@@ -35,30 +27,30 @@ class DataService @Inject constructor(private val httpClient: HttpClient) : Data
                     setBody(group)
                 }
                 when (response.status) {
-                    Created -> {
+                    HttpStatusCode.Companion.Created -> {
                         val responseBody = response.body<Group>()
-                        Log.d(TAG, "DataService | Product added: $responseBody")
+                        Log.d(ContentValues.TAG, "DataService | Product added: $responseBody")
                         emit(Result.success(responseBody))
                     }
 
-                    Found -> {
+                    HttpStatusCode.Companion.Found -> {
                         val responseBody = response.body<Group>()
-                        Log.d(TAG, "DataService | Product already exists: $responseBody")
+                        Log.d(ContentValues.TAG, "DataService | Product already exists: $responseBody")
                         emit(Result.success(responseBody))
                     }
 
-                    NotFound -> {
-                        Log.d(TAG, "DataService | Error with status code: ${response.status}")
+                    HttpStatusCode.Companion.NotFound -> {
+                        Log.d(ContentValues.TAG, "DataService | Error with status code: ${response.status}")
                         emit(Result.failure(RuntimeException("Error occurred: Server responded with status code ${response.status}")))
                     }
 
                     else -> {
-                        Log.d(TAG, "DataService | Unexpected response status: ${response.status}")
+                        Log.d(ContentValues.TAG, "DataService | Unexpected response status: ${response.status}")
                         emit(Result.failure(Exception("Unexpected response status: ${response.status}")))
                     }
                 }
             } catch (e: Exception) {
-                Log.d(TAG, "DataService | Failed to post product: ${e.message}")
+                Log.d(ContentValues.TAG, "DataService | Failed to post product: ${e.message}")
                 emit(Result.failure(e))
             }
         }
@@ -68,10 +60,10 @@ class DataService @Inject constructor(private val httpClient: HttpClient) : Data
         return flow {
             try {
                 val response: HttpResponse = httpClient.get(url)
-                Log.d(TAG, "ProductService | We have products: ${response.body<List<Group>>()}")
+                Log.d(ContentValues.TAG, "ProductService | We have products: ${response.body<List<Group>>()}")
                 emit(Result.success(response.body()))
             } catch (e: Exception) {
-                Log.d(TAG, "ProductService | We have no products: ${e.message}")
+                Log.d(ContentValues.TAG, "ProductService | We have no products: ${e.message}")
                 emit(Result.failure(e))
             }
         }
